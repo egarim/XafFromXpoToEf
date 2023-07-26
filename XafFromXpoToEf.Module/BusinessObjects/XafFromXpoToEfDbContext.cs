b1f393d6982b94b0d96cf44e1f598208229c8c11
Jose Ojeda <joche.ojeda@bitframeworks.com>
2023-07-27 00:24:18 +0300

    EnableSensitiveDataLogging

--- XafFromXpoToEf.Module/BusinessObjects/XafFromXpoToEfDbContext.cs
+++ XafFromXpoToEf.Module/BusinessObjects/XafFromXpoToEfDbContext.cs
@@ -79,29 +79,34 @@ public class XafFromXpoToEfEFCoreDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder
-            //HACK log to any action that accepts a string message as parameter
 
-            .LogTo((message) =>
-            {
-                ///HACK log to Debug.WriteLine but you can write it to a file if needed
-                Debug.WriteLine(message);
-            });
+        //HACK log to any action that accepts a string message as parameter
+        optionsBuilder
+        .LogTo((message) =>
+         {
+             ///HACK log to Debug.WriteLine but you can write it to a file if needed
+             Debug.WriteLine(message);
+         });
 
         //HACK there are several type of loggers, this is just one of them, check the nugets that start with the name Microsoft.Extensions.Logging
         //For example, Microsoft.Extensions.Logging.Console and Microsoft.Extensions.Logging.Debug
-        
-        //var loggerFactory = LoggerFactory.Create(builder =>
-        //{
-        //    builder
-        //        //HACK extra filters https://learn.microsoft.com/en-us/ef/core/logging-events-diagnostics/simple-logging#custom-filters
-        //        .AddFilter((category, level) =>
-        //            category == DbLoggerCategory.Database.Command.Name
-        //        && level == LogLevel.Information)
-        //        .AddDebug();
-        //});
+
+        var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                //HACK extra filters https://learn.microsoft.com/en-us/ef/core/logging-events-diagnostics/simple-logging#custom-filters
+                .AddFilter((category, level) =>
+                    category == DbLoggerCategory.Database.Command.Name
+                && level == LogLevel.Information)
+                .AddDebug();
+        });
+
+
 
         //optionsBuilder.UseLoggerFactory(loggerFactory);
+
+        //HACK enable sensitive data logging EnableSensitiveDataLogging
+        optionsBuilder.UseLoggerFactory(loggerFactory).EnableSensitiveDataLogging();
 
     }
 
